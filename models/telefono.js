7d0fff32b15dd2fd5f6e29537d27e89de799e56b
Ezequiel Weiss <38471904+ezeweiss@users.noreply.github.com>
2025-02-27 15:32:07 -0300

    agrego manejo de telefonos en back

---
+++ models/telefono.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../datos/db");
+const Configuracion = require("./configuracion");
+
+const Telefono = sequelize.define("Telefonos", {
+    ip: { type: DataTypes.STRING, allowNull: false },
+    interno: { type: DataTypes.STRING, allowNull: false },
+    usuario: { type: DataTypes.STRING, allowNull: true },
+    clave: { type: DataTypes.STRING, allowNull: true },
+    sipUser: { type: DataTypes.STRING, allowNull: true },
+    sipPassword: { type: DataTypes.STRING, allowNull: true },
+    sector: { type: DataTypes.STRING, allowNull: true },
+    numeroSerie: { type: DataTypes.STRING, allowNull: true },
+    mac: { type: DataTypes.STRING, allowNull: false },
+    id_config: { 
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'Configuraciones',
+            key: 'id',
+        },
+        allowNull: false,
+    },
+}, {
+    schema: 'Equipos',
+    timestamps: false,
+});
+
+Telefono.belongsTo(Configuracion, { foreignKey: 'id_config', as: 'configuracion' });
+
+sequelize.sync({ force: false }).then(() => {
+    console.log('Base de datos sincronizada');
+}).catch(err => {
+    console.error('Error al sincronizar base de datos:', err);
+});
+
+module.exports = Telefono;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const equiposRoutes = require("./routes/equipos");
 const direccionesRoutes = require("./routes/direcciones");
 const marcasRoutes = require("./routes/marcas");
 const switchesRoutes = require("./routes/switches");
+const telefonosRoutes = require("./routes/telefonos");
 
 const app = express();
 app.use(cors());
@@ -13,6 +14,7 @@ app.use("/equipos", equiposRoutes);
 app.use("/direcciones", direccionesRoutes);
 app.use("/marcas", marcasRoutes); 
 app.use("/switches", switchesRoutes);
+app.use("/telefonos", telefonosRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
